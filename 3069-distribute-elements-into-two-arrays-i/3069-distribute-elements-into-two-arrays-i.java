9610f64ae2c0212abd22684daddc3c1c07f23fe5
Naman Seth <sethumans@gmail.com>
2026-08-22 23:47:19 +0530

    Time: 1 ms (99.64%), Space: 46.7 MB (38.12%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -1,24 +1,23 @@
 class Solution {
     public int[] resultArray(int[] nums) {
         int n = nums.length;
-        List<Integer> arr1 = new ArrayList<>();
-        List<Integer> arr2 = new ArrayList<>();
-        arr1.add(nums[0]);
-        arr2.add(nums[1]);
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
+        int p1 = 0, p2 = 0;
+        arr1[p1++] = nums[0];
+        arr2[p2++] = nums[1];
         for (int i = 2; i < n; i++) {
-            if (arr2.get(arr2.size()-1) > arr1.get(arr1.size() - 1)) {
-                arr2.add(nums[i]);
+            if (arr1[p1 - 1] > arr2[p2 - 1]) {
+                arr1[p1] = nums[i];
+                p1++;
             } else {
-                arr1.add(nums[i]);
+                arr2[p2] = nums[i];
+                p2++;
             }
         }
-
         int[] res = new int[n];
-        System.arraycopy(arr1.stream()
-                .mapToInt(Integer::intValue).toArray(), 0, res, 0, arr1.size());
-        System.arraycopy(arr2.stream()
-                .mapToInt(Integer::intValue).toArray(), 0, res, arr1.size(), arr2.size());
+        System.arraycopy(arr1, 0, res, 0, p1);
+        System.arraycopy(arr2, 0, res, p1, p2);
         return res;
-
     }
 }
