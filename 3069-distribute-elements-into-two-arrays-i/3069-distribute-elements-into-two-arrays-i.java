0e51e79f7787fb36d37479b8937c23f616e3b688
Naman Seth <sethumans@gmail.com>
2026-08-22 23:54:51 +0530

    Time: 1 ms (99.64%), Space: 46.6 MB (56.84%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -4,20 +4,25 @@ class Solution {
         int[] arr1 = new int[n];
         int[] arr2 = new int[n];
         int p1 = 0, p2 = 0;
-        arr1[p1++] = nums[0];
-        arr2[p2++] = nums[1];
-        for (int i = 2; i < n; i++) {
-            if (arr1[p1 - 1] > arr2[p2 - 1]) {
-                arr1[p1] = nums[i];
-                p1++;
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                arr1[p1++] = nums[i];
+
+            } else if (i == 1) {
+                arr2[p2++] = nums[i];
+
             } else {
-                arr2[p2] = nums[i];
-                p2++;
+
+                if (arr1[p1 - 1] > arr2[p2 - 1]) {
+                    arr1[p1++] = nums[i];
+                } else {
+                    arr2[p2++] = nums[i];
+                }
             }
         }
-        int[] res = new int[n];
-        System.arraycopy(arr1, 0, res, 0, p1);
-        System.arraycopy(arr2, 0, res, p1, p2);
-        return res;
+        for (int i = 0; i < p2; i++) {
+            arr1[p1++] = arr2[i];
+        }
+        return arr1;
     }
 }
