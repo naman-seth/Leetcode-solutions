79bf575619fbdda6e693089c8ce0d72a61d0e44d
Naman Seth <sethumans@gmail.com>
2026-08-28 18:10:09 +0530

    1904-SECOND-LARGEST-DIGIT-IN-A-STRING Stats: Time: 1 ms (97.22%), Space: 43.4 MB (69.77%) - LeetHub

--- 1904-second-largest-digit-in-a-string/1904-second-largest-digit-in-a-string.java
+++ 1904-second-largest-digit-in-a-string/1904-second-largest-digit-in-a-string.java
@@ -1,15 +1,16 @@
 class Solution {
     public int secondHighest(String s) {
-        int large=-1;
-        int secLarge=-1;
-        for(char ch:s.toCharArray()){
-            if(ch>='0' && ch<='9'){
-                int dig=ch-'0';
-                if(dig>large){
-                    secLarge=large;
-                    large=dig;
-                }else if(dig>secLarge && dig<large){
-                    secLarge=dig;
+        int large = -1;
+        int secLarge = -1;
+        int dig = 0;
+        for (char ch : s.toCharArray()) {
+            dig = ch - '0';
+            if (ch >= '0' && ch <= '9') {
+                if (dig > large) {
+                    secLarge = large;
+                    large = dig;
+                } else if (dig > secLarge && dig < large) {
+                    secLarge = dig;
                 }
             }
         }
