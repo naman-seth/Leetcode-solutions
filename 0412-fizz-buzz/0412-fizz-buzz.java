25d22996e0f135ec087159f7d92443f4e597db83
Naman Seth <sethumans@gmail.com>
2026-08-31 19:03:34 +0530

    0412-FIZZ-BUZZ Stats: Time: 1 ms (99.88%), Space: 46.9 MB (26.65%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -1,17 +1,16 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> res = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
 
-            if ((i + 1) % 15 == 0) {
-                res.add(i, "FizzBuzz");
-            } else if ((i + 1) % 3 == 0) {
-                res.add(i, "Fizz");
-            } else if ((i + 1) % 5 == 0) {
-                res.add(i, "Buzz");
+            if (i % 15 == 0) {
+                res.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                res.add("Fizz");
+            } else if (i % 5 == 0) {
+                res.add("Buzz");
             } else {
-
-                res.add(i, String.valueOf(i + 1));
+                res.add(String.valueOf(i));
             }
         }
         return res;
