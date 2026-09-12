77fa98e3ec9f0093706e07e78dcc1230166a72f8
Naman Seth <sethumans@gmail.com>
2026-09-12 21:35:31 +0530

    0027-REMOVE-ELEMENT Stats: Time: 0 ms (100%), Space: 43.4 MB (78.71%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -3,9 +3,7 @@ class Solution {
         int n = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
-                int temp = nums[i];
-                nums[i] = nums[n];
-                nums[n] = temp;
+                nums[n] = nums[i];
                 n++;
             }
         }
