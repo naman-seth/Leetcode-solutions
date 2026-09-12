310cded8b00daa6fd35af7fc12b3cc066aef087a
Naman Seth <sethumans@gmail.com>
2026-09-12 21:49:27 +0530

    2271-REARRANGE-ARRAY-ELEMENTS-BY-SIGN Stats: Time: 3 ms (100%), Space: 175.7 MB (28.54%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,37 +1,19 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
+        int pos=0;
+        int neg=1;
         int n = nums.length;
-        int posN = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] >= 0) {
-                posN++;
+        int[] ans = new int[n];
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                ans[pos]=nums[i];
+                pos+=2;
+            }else{
+                ans[neg]=nums[i];
+                neg+=2;
             }
-        }
-        int[] pos = new int[posN];
-        int[] neg = new int[n - posN];
-        int posP = 0;
-        int negP = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] >= 0) {
-                pos[posP] = nums[i];
-                posP++;
-            } else {
-                neg[negP] = nums[i];
-                negP++;
-            }
-        }
-        posP = 0;
-        negP = 0;
-        for (int i = 0; i < n; i++) {
-            if (i == 0) {
-                nums[i] = pos[posP++];
-            } else if (i % 2 == 0) {
-                nums[i] = pos[posP++];
-            } else {
-                nums[i] = neg[negP++];
-            }
-        }
-        return nums;
 
+        }
+        return ans;
     }
 }
